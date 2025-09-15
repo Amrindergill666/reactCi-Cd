b88c582a10ea0923eec7841bc2d7ce08b5764f6e
Amrindergill666 <gillamrinder733@gmail.com>
2025-09-15 16:46:03 +0530

    Add CI pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-    output: 'export',
-    basePath: isProd ? '/reactCi-Cd' : '',
-  assetPrefix: isProd ? '/reactCi-Cd/' : '',
-};
+const nextConfig = {};
 
 export default nextConfig;
